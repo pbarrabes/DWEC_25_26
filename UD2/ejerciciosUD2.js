34d7ddd9e4127eb1efedaee0b55e4c1bdaf75ae1
Pablo <pablo.barrabes@fppiramide.com>
2025-10-07 14:19:13 +0200

    Hasta 25 UD 2

--- UD2/ejerciciosUD2.js
+++ UD2/ejerciciosUD2.js
@@ -92,7 +92,165 @@ console.log([...string1.matchAll(string2)].length); */
 /* Ejercicio 12 Usa el operador “?” para escribir un código que nos pida un número y nos muestre un mensaje si es mayor o igual que cero 
 y otro mensaje si es menor estricto que cero. */
 
-let numero = prompt("Die un numero entero");
+/* let numero = prompt("Die un numero entero");
 (numero>=0)?alert("El numero es positivo o cero"):alert("el numero es negativo");
+ */
+
+/* Ejercicio 13 y 14 Crea un programa  utilizando la sentencia if else  que pida al usuario que introduzca una edad y muestre el siguiente mensaje en función del número introducido:
+0-12: Niño
+13-26: Joven
+27-65: Adulto
+>65: Jubilado
+ */
+/* 
+let edad;
+do {
+    console.log(edad);
+} while (!(edad=parseInt(prompt("Dime tu edad"))));
 
 
+    if(edad>=0&&edad<=12) {
+        alert("Eres un Niño");
+    } else if(edad<=26) {
+        alert("Eres un Joven");
+    } else if(edad <=65){
+        alert("Eres un adulto");
+    } else{
+        alert("Eres un jubilado")
+}; */
+
+/*  15 Construye un programa que pida   un número del 1 al 10 al usuario, luego muestra por pantalla la tabla de multiplicar de ese número hasta 10. 
+    Para ello muestra en líneas diferentes el siguiente mensaje: “ 8 por 1 = 8” y así sucesivamente. Suponiendo que el usuario hubiera introducido un 8.
+
+ */
+
+
+    let numero= parseInt(prompt("Dime un numero"));
+    for (let i = 0; i <= 10;i++){
+        document.write(`<p>${numero} por ${i} = ${numero * i}</p>`);
+    } 
+
+        /* 16 y 17 Crea un programa que sea un “pequeño traductor” de nombres de frutas. 
+A partir de una lista de 5 Frutas en español (“Pera”, “Manzana”,”Piña”,”Fresa”,”Naranja”) 
+el programa pedirá al usuario el nombre de una fruta, en el caso de que corresponda al listado le mostrará en un cuadro de diálogo la traducción al Inglés. 
+En el caso de otro texto mostrará “Fruta desconocida”. Para este ejercicio debes usar una estructura switch.
+
+Comenta el código con los comentarios que estimes necesarios.
+ */
+
+ 
+// Esta solucion muestra como incluir varis casos al no poner el break en cada case.
+// Otras opciones para el 17 pasarian por pasar fruta a minusculas. En la linea de abajo se muestra con el metodo que tiene en cuenta la localizacion. 
+//let fruta = prompt("Dime un nombre de fruta").toLowerCase() ; 
+let fruta=prompt("Dime un nombre de fruta");
+
+switch (fruta) {
+    case "Pera":
+    case "pera":// Lo incluyo tambien para la primera en minuscula
+        alert(`${fruta} en ingles se dice Pear`);    
+        break;
+    case "Manzana":
+    case "manzana":
+        alert(`${fruta} en ingles se dice Apple`);    
+        
+        break;
+
+    default:
+        alert(`No se como se dice ${fruta} en ingles`);
+        break;
+} 
+
+/* 20 Escribe tres funciones suma que acepte un número indeterminado de argumentos y devuelva la suma de todos ellos. 
+Debes evitar que la función devuelva “undefined” si no se le pasa ningún argumento, en cuyo caso el valor que devuelva tiene que ser 0. 
+Debes escribir cada versión de la función  de las tres formas que hemos visto.
+suma1 , declaración tradicional de una función
+suma2, función como expresión.
+suma3, función flecha. */
+
+/* function suma(a=0,...resto){
+    let suma=parseInt(a);
+    for (const dato of resto) {//Por ejemplo con for of. Hay muchas maneras de hacerlo.
+        suma+=parseInt(dato);
+    }
+    return isNaN(suma)?"Error":suma;
+}
+
+let suma2= function(a=0,...resto){
+        let suma=parseInt(a);
+    for (const dato of resto) {//Por ejemplo con for of. Hay muchas maneras de hacerlo.
+        suma+=parseInt(dato);
+    }
+    return isNaN(suma)?"Error":suma;
+}
+
+let suma3= (a=0,...resto)=>{
+        let suma=parseInt(a);
+    for (const dato of resto) {//Por ejemplo con for of. Hay muchas maneras de hacerlo.
+        suma+=parseInt(dato);
+    }
+    return isNaN(suma)?"Error":suma;
+}
+console.log(suma());
+console.log(suma(2,5,7,9));
+console.log(suma("2",3));
+console.log(suma("er ",2)); */
+
+ /*21 Escribe una función denominada “operacionesEncadenadas” con los siguientes argumentos
+Dos valores que actuarán de datos para las operaciones.
+El tercero y cuarto dos funciones callback que definiremos aparte. La primera devolverá la suma de los dos datos. La segunda el producto
+El último argumento será una función callback que pasaremos en la llamada a la función  como función flecha con un mensaje por pantalla al usuario */
+
+/* let a =parseInt(prompt("Damee un numero"));
+let b =parseInt(prompt("Damee otro numero"));
+
+let operacionesEncadenadas= function(x,y,operacion1,operacion2,final){
+
+    document.write(operacion1(x,y));
+    document.write(operacion2(x,y));
+    final();
+}
+
+function suma(op1,op2){
+    return op1+op2;
+}
+
+let producto= (x=1,y)=> x*y;
+
+operacionesEncadenadas(a,b,suma,producto,()=>alert("Hasta luego")); */
+
+/* 22 Crea una función que sume todos los elementos de un array numérico.(Sin recorrer el Array) */
+/* 
+function sumaArray(array){
+    return array.reduce((suma,valorActual)=> suma+valorActual)
+}
+
+document.write(sumaArray([1,2,3]));
+
+/* 23 Encuentra y muestra todos los números pares de un array. (Sin recorrer el Array)*/
+
+/* const array=[2,5,7,3,4,6,44,67];
+let buscarPares=(acumulador,numeroActual)=>{
+    numeroActual%2==0?document.write(`${numeroActual}<br>`):"";
+}
+let numPares=array.reduce(buscarPares);    */
+
+
+// otra opcion mas limpia
+//document.write(`${array.filter((numero)=>numero%2==0)}`);// document.write muestra el array con sus elementos separados por ,
+// Debajo lo mismo pero menos compacto.
+
+/*  24 Encuentra la palabra más larga en un array de palabras. */
+
+const palabras=["hola","perro","caracola","jes"];
+//Ahora si otra vez abusando del reduce
+
+/* let palabraMaslarga=palabras.reduce(
+    (masLarga,palabraActual)=>palabraActual.length>=masLarga.length? palabraActual: masLarga);
+document.write(palabraMaslarga); */
+
+/* 25  Elimina los elementos duplicados de un array. (Usa la estructura Set como ayuda)*/
+
+const array=["perro","gato","caballo","perro","caballo"];
+const arrayAset= new Set(array);
+const arraySinDuplicados= Array.from(arrayAset);
+console.log(arraySinDuplicados);
